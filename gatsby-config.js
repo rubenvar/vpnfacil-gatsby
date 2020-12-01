68082c6175006f491b2f00ec83f995fc3fa4a692
Rubén <hey@rubenvara.io>
2020-12-01 13:10:03 +0100

    add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,21 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+//* See: https://www.gatsbyjs.com/docs/gatsby-config/
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: `VPNFácil`,
+    author: `@rubenvara01`,
+    description: `Elige tu VPN aquí`,
+    siteUrl: `https://vpnfacil.com`,
+  },
+  plugins: [
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
+    // `gatsby-transformer-sharp`,
+    // `gatsby-plugin-sharp`,
+  ],
+};
